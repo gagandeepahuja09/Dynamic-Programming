e920d7a6b61397c6d16688ed297835d5b3399fc3
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-05-31 10:29:57 +0530

    easy

--- Interview Bit/DistinctSubsequences.cpp
+++ Interview Bit/DistinctSubsequences.cpp
@@ -1,20 +1,19 @@
-int numOfSub(string A, string B, vector<vector<int>>& dp) {
-    int ans = 0;
-    if(B.length() == 0)
+int distSubs(string s, string t, int i, int j, int m, int n, vector<vector<int>>& dp) {
+    if(j == n)
         return 1;
-    if(A.length() == 0)
+    if(i == m)
         return 0;
-    if(dp[A.length()][B.length()] != -1)
-        return dp[A.length()][B.length()];
-    if(A[0] == B[0]) {
-        ans = ans + numOfSub(A.substr(1), B.substr(1), dp); 
+    if(dp[i][j] != -1)
+        return dp[i][j];
+    if(s[i] == t[j]) {
+        return dp[i][j] = distSubs(s, t, i + 1, j + 1, m, n, dp) + 
+        distSubs(s, t, i + 1, j, m, n, dp);
     }
-    ans = ans + numOfSub(A.substr(1), B, dp);
-    return dp[A.length()][B.length()] = ans;
+    return dp[i][j] = distSubs(s, t, i + 1, j, m, n, dp);
 }
 
 int Solution::numDistinct(string A, string B) {
-    vector<vector<int>> dp(A.length() + 1, vector<int>(B.length() + 1, -1));
-    return numOfSub(A, B, dp);    
+    vector<vector<int>> dp(A.size() + 1, vector<int>(B.size() + 1, -1));
+    return distSubs(A, B, 0, 0, A.size(), B.size(), dp);
 }
 
