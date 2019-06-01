536a82745389efee1f2f6fa7b380b4d60cb76f2a
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-01 12:14:36 +0530

    easy

--- Interview Bit/CoinSumInfinite.cpp
+++ Interview Bit/CoinSumInfinite.cpp
@@ -1,29 +1,21 @@
 #define MOD 1000007
 
-/*int coinChange(vector<int> &A, int n, int m, vector<vector<int>>& dp) {
-    if(m < 0)
-        return 0;
-    if(n < 0)
-        return 0;
-    if(n == 0)
+int coinWays(vector<int> &A, int m, int i) {
+    if(m == 0)
         return 1;
-    if(dp[m][n] != -1)
-        return dp[m][n];
-    int ans = 0;    
-    ans = (coinChange(A, n - A[m], m, dp)%MOD + coinChange(A, n, m-1, dp)%MOD)%MOD;
-    ans = ans%MOD;
-    return dp[m][n] = ans; 
-}*/
+    if(i == A.size() || m < 0)
+        return 0;
+    int ans = coinWays(A, m, i + 1) + coinWays(A, m - A[i], i);
+    return ans;
+}
+
 int Solution::coinchange2(vector<int> &A, int B) {
-    int m = A.size();
-    // vector<vector<int>> dp(m, vector<int>(B+1, -1));
-    vector<int> dp(B+1, 0);
-    // return coinChange(A, B, m - 1, dp)%MOD;
+    int n = A.size();
+    vector<int> dp(B + 1, 0);
     dp[0] = 1;
-    for(int i=0; i<A.size(); i++) {
+    for(int i = 0; i < n; i++) {
         for(int j = A[i]; j <= B; j++) {
-            dp[j] = dp[j] + dp[j - A[i]];
-            dp[j] %= MOD;
+            dp[j] = (dp[j] % MOD + dp[j - A[i]] % MOD) % MOD;
         }
     }
     return dp[B];
