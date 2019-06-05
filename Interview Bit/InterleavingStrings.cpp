216ea86497377d4ab2d4cd5d8d28d9d497bda239
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-05 13:16:40 +0530

    eSY

--- Interview Bit/InterleavingStrings.cpp
+++ Interview Bit/InterleavingStrings.cpp
@@ -1,21 +1,21 @@
-int interleavingString(string A, string B, string C, 
-vector<vector<int>>& dp) {
-    if(A.length() == 0 && B.length() == 0 && C.length() == 0)
+int interleave(string A, string B, string C, vector<vector<int>>& dp) {
+    if(!C.size() && !A.size() && !B.size())
         return 1;
-    if(C.length() == 0)
+    if(!C.size())
         return 0;
-    if(dp[A.length()][B.length()] != -1)
-        return dp[A.length()][B.length()];
-    int option1 = false, option2 = false;    
+    if(dp[A.size()][B.size()] != -1)
+        return dp[A.size()][B.size()];
+    int ans = 0;
     if(A[0] == C[0])
-        option1 = interleavingString(A.substr(1), B, C.substr(1), dp);
+        ans = ans || interleave(A.substr(1), B, C.substr(1), dp);
     if(B[0] == C[0])
-        option2 = interleavingString(A, B.substr(1), C.substr(1), dp);
-    return dp[A.length()][B.length()] = (option1 || option2);
+        ans = ans || interleave(A, B.substr(1), C.substr(1), dp);
+    return dp[A.size()][B.size()] = ans;    
 }
 
 int Solution::isInterleave(string A, string B, string C) {
-    vector<vector<int>> dp(A.length() + 1, vector<int>(B.length() + 1, -1));
-    return interleavingString(A, B, C, dp);
+    vector<vector<int>> dp(A.size() + 1, vector<int>(B.size() + 1, -1));
+    return interleave(A, B, C, dp);
 }
+
 
