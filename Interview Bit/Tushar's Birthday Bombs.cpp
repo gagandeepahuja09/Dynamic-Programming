7532187ed5d5617c2f687fb08e52cac5fb2ffd79
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-03 11:13:55 +0530

    new and imp concept

---
+++ Interview Bit/Tushar's Birthday Bombs.cpp
@@ -0,0 +1,24 @@
+vector<int> Solution::solve(int A, vector<int> &B) { 
+    int mn = INT_MAX, pos = -1;
+    for(int i = 0; i < B.size(); i++) {
+        if(B[i] < mn) {
+            mn = B[i];
+            pos = i;
+        }
+    }
+    int maxKick = A / mn;
+    vector<int> ret;
+    for(int i = 0; i < maxKick; i++)
+        ret.push_back(pos);
+    int rem = A - (maxKick * mn);
+    int i = 0, j = 0;
+    while(i < maxKick) {
+        pos = ret[i];
+        int idx = -1;
+        for(j = 0; j < n; j++) {
+            
+        }
+    }
+    return ret;
+}
+
