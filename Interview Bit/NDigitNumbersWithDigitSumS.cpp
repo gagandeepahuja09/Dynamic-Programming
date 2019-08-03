86ea0c0fe1109b89be9a1a659e75dec25a041dd3
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-03 15:18:34 +0530

    very easy

--- Interview Bit/NDigitNumbersWithDigitSumS.cpp
+++ Interview Bit/NDigitNumbersWithDigitSumS.cpp
@@ -1,27 +1,23 @@
 #define MOD 1000000007
 
-int noOfWays(int N, int S, vector<vector<int>>& dp) {
-    if(N == 0 && S == 0)
-        return 1;
+int f(int N, int S, vector<vector<int>>& dp) {
     if(N == 0)
-        return 0;
-    if(S == 0)
-        return 1;
+        return (S == 0);
     if(dp[N][S] != -1)
         return dp[N][S];
-    int ans = 0;    
-    for(int i=0; i <= 9 && i <= S; i++) {
-        ans = (ans%MOD + noOfWays(N-1, S - i, dp)%MOD)%MOD;
+    int ret = 0;
+    for(int i = 0; i <= 9 && i <= S; i++) {
+        ret = (ret % MOD + f(N - 1, S - i, dp) % MOD) % MOD;
     }
-    return dp[N][S] = ans%MOD;
+    return dp[N][S] = ret % MOD;
 }
 
-int Solution::solve(int N, int S) {
-    int ans = 0;  
-    vector<vector<int>> dp(N+1, vector<int>(S+1, -1));
-    for(int i=1; i <= 9 && i <= S; i++) {
-        ans = (ans%MOD + noOfWays(N-1, S - i, dp)%MOD)%MOD;
+int Solution::solve(int A, int B) {
+    int ret = 0;
+    vector<vector<int>> dp(A + 1, vector<int>(B + 1, - 1));
+    for(int i = 1; i <= 9 && i <= B; i++) {
+        ret = (ret % MOD + f(A - 1, B - i, dp) % MOD) % MOD;
     }
-    return ans%MOD;
+    return ret % MOD;
 }
 
