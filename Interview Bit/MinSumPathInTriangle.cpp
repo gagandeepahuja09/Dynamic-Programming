f445959b7480226380180a3817594acef429f4ce
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-05 16:39:59 +0530

    reducing space + going bottom to avoid cases

--- Interview Bit/MinSumPathInTriangle.cpp
+++ Interview Bit/MinSumPathInTriangle.cpp
@@ -1,39 +1,13 @@
-/*int minPath(vector<vector<int>>& A, int i, int j, int r, 
-vector<vector<int>>& dp) {
-    int c = i;
-    if(j > c)
-        return INT_MAX;
-    if(i == r + 1)
-        return 0;
-    if(dp[i][j] != -1)
-        return dp[i][j];
-    int ans;    
-    int option1 = minPath(A, i + 1, j + 1, r, dp);
-    int option2 = minPath(A, i + 1, j, r, dp);
-    if(option1 == INT_MAX)
-        ans = option2;
-    else if(option2 == INT_MAX)
-        ans = option1;
-    else
-        ans = A[i][j] + min(option1, option2);
-    return dp[i][j] = ans;    
-}*/
-
 int Solution::minimumTotal(vector<vector<int> > &A) {
-    // Recursive Approach
-    // vector<vector<int>> dp(A.size(), vector<int>(A.size(), -1));
-    // return minPath(A, 0, 0, A.size() - 1, dp);
-    
-    // Bottom Up Manner
-    int n = A.size();
-    int memo[n];
-    for(int i = 0; i < A[n - 1].size(); i++)
-        memo[i] = A[n - 1][i];
+    int n = A.size(), m = A[n - 1].size();
+    vector<int> dp(m);
+    for(int i = 0; i < m; i++)
+        dp[i] = A[n - 1][i];
     for(int i = n - 2; i >= 0; i--) {
         for(int j = 0; j < A[i].size(); j++) {
-            memo[j] = A[i][j] + min(memo[j], memo[j + 1]);
+                dp[j] = min(dp[j], dp[j + 1]) + A[i][j];
         }
     }
-    return memo[0];
+    return dp[0];
 }
 
