ccaa2b6e7769909f9d201551d8c50ddec0cb20db
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-10 14:21:50 +0530

    easy

--- Geeks For Geeks/Number of ways of scoring R runs in B balls with at most W wickets.cpp
+++ Geeks For Geeks/Number of ways of scoring R runs in B balls with at most W wickets.cpp
@@ -3,23 +3,28 @@ using namespace std;
 
 #define MOD 1000000007
 
+int dp[66][11][11];
+
 int f(int R, int W, int B) {
 	if(W < 0)
 		return 0;
 	if(B == 0)
 		return (R == 0);
+	if(dp[R][W][B] != -1)
+		return dp[R][W][B];
 	int ans = f(R, W - 1, B - 1) % MOD;
 	for(int i = 0; i <= 4; i++) {
 		ans += f(R - i, W, B - 1);
 		ans %= MOD;
 	}
 	ans += f(R - 6, W, B - 1);
-	return ans % MOD;
+	return dp[R][W][B] = ans % MOD;
 }
 
 int main() {
 	int t;
 	cin >> t;
+	memset(dp, -1, sizeof dp);
 	while(t--) {
 		int R, W, B;
 		cin >> R >> B >> W;
