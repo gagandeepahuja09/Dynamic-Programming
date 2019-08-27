788c36ed9553946c4cdd3bf0644c12a07f1b24bf
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-27 10:27:53 +0530

    good question

--- LeetCode/BurstBallons.cpp
+++ LeetCode/BurstBallons.cpp
@@ -1,25 +1,23 @@
 class Solution {
-public:
+private:
     int dp[501][501];
-    int maxCoins(vector<int>& nums, int l, int r) {
-        if(l > r)
-            return 0;
-        if(l == r) {
-            return nums[l - 1] * nums[l] * nums[l + 1];
-        }
+public:
+    int f(vector<int>& nums, int l, int r) {
+        if(l == r)
+            return nums[l - 1] * nums[l]  * nums[l + 1];
         if(dp[l][r] != -1)
             return dp[l][r];
+        int ans = 0;
         for(int i = l; i <= r; i++) {
-            dp[l][r] = max(dp[l][r], nums[i] * nums[l - 1] * nums[r+1] 
-            + maxCoins(nums, l, i - 1) + maxCoins(nums, i + 1, r));
+            ans = max(ans, f(nums, l, i - 1) + f(nums, i + 1, r) + nums[l - 1] * nums[i] * nums[r + 1]);
         }
-        return dp[l][r];
+        return dp[l][r] = ans;
     }
     
     int maxCoins(vector<int>& nums) {
         memset(dp, -1, sizeof dp);
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        return maxCoins(nums, 1, nums.size() - 2);
+        return f(nums, 1, nums.size() - 2);        
     }
 };
