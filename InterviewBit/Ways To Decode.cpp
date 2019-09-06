41ae12d85c52c8e1db3f9001515161bdfeb7f547
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-09-06 09:21:01 +0530

    easy revise

--- InterviewBit/Ways To Decode.cpp
+++ InterviewBit/Ways To Decode.cpp
@@ -1,21 +1,24 @@
-int f(string s, int i, vector<int>& dp) {
-    if(i >= s.size())
+int dp[100001];
+
+int f(string s, int i) {
+    if(i == s.size())
         return 1;
     if(dp[i] != -1)
         return dp[i];
-    int option1 = 0, option2 = 0;
-    if(s[i] != '0')
-        option1 = f(s, i + 1, dp);
-    if(s.size() - i >= 2) {
-        string curr = s.substr(i, 2);
-        if(curr >= "10" && curr <= "26")
-            option2 = f(s, i + 2, dp);
+    int ans = 0;
+    if(s[i] >= '1' && s[i] <= '9')
+        ans += f(s, i + 1);
+    if(i < s.size() - 1) {    
+        string sub = s.substr(i, 2);
+        if(sub >= "10" && sub <= "26") {
+            ans += f(s, i + 2);
+        }
     }
-    return dp[i] = option1 + option2;
+    return ans;
 }
 
 int Solution::numDecodings(string A) {
-    vector<int> dp(A.size() + 1, -1);
-    return f(A, 0, dp);
+    memset(dp, -1, sizeof dp);
+    return f(A, 0);
 }
 
