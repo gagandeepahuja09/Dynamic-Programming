b448d2ccede7bb07aec40d471e73903e2450a152
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2019-11-12 20:08:07 +0530

    visualize bottom up(a bit similar & a bit diff) from mcm

--- LeetCode/Minimum Cost Tree From Leaves.cpp
+++ LeetCode/Minimum Cost Tree From Leaves.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int dp[41][41];
     int mx[41][41];
-    
+    int dp[41][41];
     int f(int i, int j) {
         if(i == j)
             return 0;
-        int ret = 1 << 30;
         if(dp[i][j] != -1)
             return dp[i][j];
-        
+        int ans = 1e8;
         for(int k = i; k < j; k++) {
-            ret = min(ret, mx[i][k] * mx[k + 1][j] + f(i, k) + f(k + 1, j));
+            ans = min(ans, mx[i][k] * mx[k + 1][j] + f(i, k) + f(k + 1, j));
         }
-        return dp[i][j] = ret;
+        return dp[i][j] = ans;
     }
-    
     int mctFromLeafValues(vector<int>& arr) {
+        int n = arr.size();
         memset(dp, -1, sizeof dp);
-        for(int i = 0; i < arr.size(); i++) {
+        for(int i = 0; i < n; i++) {
             mx[i][i] = arr[i];
-            for(int j = i + 1; j < arr.size(); j++) {
-                mx[i][j] = max(mx[i][j - 1], arr[j]);
+            for(int j = i + 1; j < n; j++) {
+                mx[i][j] = max(arr[j], mx[i][j - 1]);
+                // cout << mx[i][j] << endl;
             }
         }
-        return f(0, arr.size() - 1);
+        return f(0, n - 1);
     }
 };
