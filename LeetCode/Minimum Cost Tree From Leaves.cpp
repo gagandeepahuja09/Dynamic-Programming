932f057dc39b7a2749b8d61db37e811ed96612ff
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-01-22 14:01:48 +0530

    revise easy can be optimized

--- LeetCode/Minimum Cost Tree From Leaves.cpp
+++ LeetCode/Minimum Cost Tree From Leaves.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    int mx[41][41];
     int dp[41][41];
-    int f(int i, int j) {
-        if(i == j)
+    int f(vector<int>& A, vector<vector<int>>& mx, int i, int j) {
+        if(j <= i)
             return 0;
+        if(j - i == 1) {
+            return A[i] * A[j];
+        }
         if(dp[i][j] != -1)
             return dp[i][j];
-        int ans = 1e8;
+        int ans = INT_MAX;
         for(int k = i; k < j; k++) {
-            ans = min(ans, mx[i][k] * mx[k + 1][j] + f(i, k) + f(k + 1, j));
+            int curr = mx[i][k] * mx[k + 1][j] + f(A, mx, i, k) + f(A, mx, k + 1, j);
+            ans = min(ans, curr);
         }
         return dp[i][j] = ans;
     }
-    int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size();
+    
+    int mctFromLeafValues(vector<int>& A) {
+        int n = A.size();
         memset(dp, -1, sizeof dp);
+        vector<vector<int>> mx(n, vector<int>(n, INT_MAX));
         for(int i = 0; i < n; i++) {
-            mx[i][i] = arr[i];
+            int currMax = A[i];
+            mx[i][i] = currMax;
             for(int j = i + 1; j < n; j++) {
-                mx[i][j] = max(arr[j], mx[i][j - 1]);
-                // cout << mx[i][j] << endl;
+                currMax = max(currMax, A[j]);
+                mx[i][j] = currMax;
             }
         }
-        return f(0, n - 1);
+        return f(A, mx, 0, n - 1);
     }
 };
