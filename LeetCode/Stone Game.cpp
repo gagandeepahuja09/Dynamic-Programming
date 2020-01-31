736e32519a288a35ea3bb6452955ed8c1c845cba
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-01-31 10:05:01 +0530

    revised

--- LeetCode/Stone Game.cpp
+++ LeetCode/Stone Game.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int dp[501][501][2];
-    int f(vector<int>& A, int l, int r, bool alexTurn) {
-        if(l > r) {
+    int dp[501][501];
+    int f(vector<int>& A, int i, int j, bool alexTurn) {
+        if(i > j) {
             return 0;
         }
-        if(dp[l][r][alexTurn] != -1)
-            return dp[l][r][alexTurn];
+        if(dp[i][j] != -1)
+            return dp[i][j];
         int score = 0;
         if(alexTurn) {
-            score = max(f(A, l + 1, r, !alexTurn) + A[l], f(A, l, r - 1, !alexTurn) + A[r]);
+            int op1 = A[i] + f(A, i + 1, j, !alexTurn);
+            int op2 = A[j] + f(A, i, j - 1, !alexTurn);
+            score = max(op1, op2);
         }
         else {
-            score = min(f(A, l + 1, r, !alexTurn) - A[l], f(A, l, r - 1, !alexTurn) - A[r]);
+            int op1 = -A[i] + f(A, i + 1, j, !alexTurn);
+            int op2 = -A[j] + f(A, i, j - 1, !alexTurn);
+            score = min(op1, op2);    
         }
-        return dp[l][r][alexTurn] = score;
+        return dp[i][j] = score;
     }
     
     bool stoneGame(vector<int>& piles) {
         memset(dp, -1, sizeof dp);
         return f(piles, 0, piles.size() - 1, 1) >= 0;
-        // return true;
     }
 };
