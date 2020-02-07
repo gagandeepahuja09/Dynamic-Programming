f7b2dd4e271c3be2b5bb0d8e66126bc9dd0a64fb
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-07 22:48:43 +0530

    IMP concept revised

--- LeetCode/Minimum Score Triangulation Of Polygon.cpp
+++ LeetCode/Minimum Score Triangulation Of Polygon.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int dp[51][51];
-    int minScore(vector<int>& A, int i, int j) {
+    int f(vector<int>& A, int i, int j) {
         if(j - i <= 1)
             return 0;
         if(dp[i][j] != -1)
             return dp[i][j];
         int ans = INT_MAX;
         for(int k = i + 1; k < j; k++) {
-            ans = min(ans, minScore(A, i, k) + minScore(A, k, j) + A[i] * A[k] * A[j]);
+            ans = min(ans, A[k] * A[j] * A[i] + f(A, i, k) + f(A, k, j));
         }
         return dp[i][j] = ans;
     }
     
     int minScoreTriangulation(vector<int>& A) {
         memset(dp, -1, sizeof dp);
-        return minScore(A, 0, A.size() - 1);
+        return f(A, 0, A.size() - 1);
     }
 };
