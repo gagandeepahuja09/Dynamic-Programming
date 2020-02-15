6881417832b979fb4f1eda839e8812eb47185209
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-15 17:42:13 +0530

    IMP revised

--- LeetCode/Arithmetic Slices II.cpp
+++ LeetCode/Arithmetic Slices II.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& A) {
-        if(A.empty())
-            return 0;
-        int ans = 0;
-        vector<map<int, int>> dp(A.size());
-        for(int i = 0; i < A.size(); i++) {
-            for(int j = 0; j < i; j++) {
-                if((long)A[i] - (long)A[j] > INT_MAX || (long)A[i] - (long)A[j] < INT_MIN)
-                    continue;
-                int diff = A[i] - A[j];
-                dp[i][diff]++;
-                dp[i][diff] += dp[j][diff];
-                ans += dp[j][diff];
+        vector<map<long, int>> dp(A.size());
+        int ans = 0, n = A.size();
+        for(int j = 1; j < n; j++) {
+            for(int i = 0; i < j; i++) {
+                long diff = (long)A[j] - (long)A[i];
+                dp[j][diff] += (1 + dp[i][diff]);
+                ans += dp[i][diff];
             }
         }
         return ans;
