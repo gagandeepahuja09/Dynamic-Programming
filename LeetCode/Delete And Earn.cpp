d2e9ad4c905e94ab31502fa6e566f58b42a090b2
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-18 10:56:07 +0530

    space optimized

--- LeetCode/Delete And Earn.cpp
+++ LeetCode/Delete And Earn.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int dp[20001];
-    int maxPoints(vector<int>& coins, int i) {
+    // int dp[20001];
+    /*int maxPoints(vector<int>& coins, int i) {
         if(i == coins.size())
             return 0;
         if(dp[i] != -1)
@@ -16,11 +16,26 @@ public:
         }
         earned += maxPoints(coins, pos);
         return dp[i] = max(skipped, earned);
-    }
+    } */
     
     int deleteAndEarn(vector<int>& nums) {
-        memset(dp, -1, sizeof dp);
+        // memset(dp, -1, sizeof dp);
         sort(nums.begin(), nums.end());
-        return maxPoints(nums, 0);
+        // return maxPoints(nums, 0);
+        int prev2 = 0, prev = 0, curr = 0, i = 0;
+        while(i < nums.size()) {
+            int pos = i;
+            curr = 0;
+            while(i < nums.size() && nums[i] == nums[pos])
+                curr += nums[i++];
+            // we can add prev if diff > 1
+            if(pos >= 1 && nums[i - 1] > nums[pos - 1] + 1)
+                curr += prev;
+            else
+                curr = max(curr + prev2, prev);
+            prev2 = prev;
+            prev = curr;
+        }
+        return curr;
     }
 };
